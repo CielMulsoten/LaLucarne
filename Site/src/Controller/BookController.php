bdbc48a6727ff555778df8eff6b62bee44b9d57f
StevenBe92 <steven.berlin@10mentionweb.org>
2018-05-24 12:18:15 +0200

    petite modif

--- Site/src/Controller/BookController.php
+++ Site/src/Controller/BookController.php
@@ -4,15 +4,7 @@ class BookController extends Controller
 {
     public function afficheAll(){
         
-        $books = $this -> getModel() -> getAllBooks();
-        $cc = new CategoryController;
-        $categories = $cc -> liste();
-        
-        $params = array(
-            'books' => $books,
-            'categories' => $categories,
-        );
-        return $this -> render('admin.html', $params);
+ $books = $this -> getModel() -> getAllBooks(); $cc = new CategoryController; $categories = $cc -> liste(); $params = array( 'books' => $books, 'categories' => $categories, ); return $this -> render('admin.html', $params);
     }
     
     public function affiche($id){
